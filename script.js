35b3ac9bf21529cc35df199bd96a56bdff34c831
test <test@gmail.com>
2024-06-19 08:45:03 +0330

    :/

---
+++ script.js
@@ -0,0 +1,902 @@
+const data = [
+  {
+    id: 1,
+    title: "title 1",
+  },
+  {
+    id: 2,
+    title: "title 2",
+  },
+  {
+    id: 3,
+    title: "title 3",
+  },
+  {
+    id: 4,
+    title: "title 4",
+  },
+  {
+    id: 5,
+    title: "title 5",
+  },
+  {
+    id: 6,
+    title: "title 6",
+  },
+  {
+    id: 7,
+    title: "title 7",
+  },
+  {
+    id: 8,
+    title: "title 8",
+  },
+  {
+    id: 9,
+    title: "title 9",
+  },
+  {
+    id: 10,
+    title: "title 10",
+  },
+  {
+    id: 11,
+    title: "title 11",
+  },
+  {
+    id: 12,
+    title: "title 12",
+  },
+  {
+    id: 13,
+    title: "title 13",
+  },
+  {
+    id: 14,
+    title: "title 14",
+  },
+  {
+    id: 15,
+    title: "title 15",
+  },
+  {
+    id: 16,
+    title: "title 16",
+  },
+  {
+    id: 17,
+    title: "title 17",
+  },
+  {
+    id: 18,
+    title: "title 18",
+  },
+  {
+    id: 19,
+    title: "title 19",
+  },
+  {
+    id: 20,
+    title: "title 20",
+  },
+  {
+    id: 21,
+    title: "title 21",
+  },
+  {
+    id: 22,
+    title: "title 22",
+  },
+  {
+    id: 23,
+    title: "title 23",
+  },
+  {
+    id: 24,
+    title: "title 24",
+  },
+  {
+    id: 25,
+    title: "title 25",
+  },
+  {
+    id: 26,
+    title: "title 26",
+  },
+  {
+    id: 27,
+    title: "title 27",
+  },
+  {
+    id: 28,
+    title: "title 28",
+  },
+  {
+    id: 29,
+    title: "title 29",
+  },
+  {
+    id: 30,
+    title: "title 30",
+  },
+  {
+    id: 31,
+    title: "title 31",
+  },
+  {
+    id: 32,
+    title: "title 32",
+  },
+  {
+    id: 33,
+    title: "title 33",
+  },
+  {
+    id: 34,
+    title: "title 34",
+  },
+  {
+    id: 35,
+    title: "title 35",
+  },
+  {
+    id: 36,
+    title: "title 36",
+  },
+  {
+    id: 37,
+    title: "title 37",
+  },
+  {
+    id: 38,
+    title: "title 38",
+  },
+  {
+    id: 39,
+    title: "title 39",
+  },
+  {
+    id: 40,
+    title: "title 40",
+  },
+  {
+    id: 41,
+    title: "title 41",
+  },
+  {
+    id: 42,
+    title: "title 42",
+  },
+  {
+    id: 43,
+    title: "title 43",
+  },
+  {
+    id: 44,
+    title: "title 44",
+  },
+  {
+    id: 45,
+    title: "title 45",
+  },
+  {
+    id: 46,
+    title: "title 46",
+  },
+  {
+    id: 47,
+    title: "title 47",
+  },
+  {
+    id: 48,
+    title: "title 48",
+  },
+  {
+    id: 49,
+    title: "title 49",
+  },
+  {
+    id: 50,
+    title: "title 50",
+  },
+  {
+    id: 51,
+    title: "title 51",
+  },
+  {
+    id: 52,
+    title: "title 52",
+  },
+  {
+    id: 53,
+    title: "title 53",
+  },
+  {
+    id: 54,
+    title: "title 54",
+  },
+  {
+    id: 55,
+    title: "title 55",
+  },
+  {
+    id: 56,
+    title: "title 56",
+  },
+  {
+    id: 57,
+    title: "title 57",
+  },
+  {
+    id: 58,
+    title: "title 58",
+  },
+  {
+    id: 59,
+    title: "title 59",
+  },
+  {
+    id: 60,
+    title: "title 60",
+  },
+  {
+    id: 61,
+    title: "title 61",
+  },
+  {
+    id: 62,
+    title: "title 62",
+  },
+  {
+    id: 63,
+    title: "title 63",
+  },
+  {
+    id: 64,
+    title: "title 64",
+  },
+  {
+    id: 65,
+    title: "title 65",
+  },
+  {
+    id: 66,
+    title: "title 66",
+  },
+  {
+    id: 67,
+    title: "title 67",
+  },
+  {
+    id: 68,
+    title: "title 68",
+  },
+  {
+    id: 69,
+    title: "title 69",
+  },
+  {
+    id: 70,
+    title: "title 70",
+  },
+  {
+    id: 71,
+    title: "title 71",
+  },
+  {
+    id: 72,
+    title: "title 72",
+  },
+  {
+    id: 73,
+    title: "title 73",
+  },
+  {
+    id: 74,
+    title: "title 74",
+  },
+  {
+    id: 75,
+    title: "title 75",
+  },
+  {
+    id: 76,
+    title: "title 76",
+  },
+  {
+    id: 77,
+    title: "title 77",
+  },
+  {
+    id: 78,
+    title: "title 78",
+  },
+  {
+    id: 79,
+    title: "title 79",
+  },
+  {
+    id: 80,
+    title: "title 80",
+  },
+  {
+    id: 81,
+    title: "title 81",
+  },
+  {
+    id: 82,
+    title: "title 82",
+  },
+  {
+    id: 83,
+    title: "title 83",
+  },
+  {
+    id: 84,
+    title: "title 84",
+  },
+  {
+    id: 85,
+    title: "title 85",
+  },
+  {
+    id: 86,
+    title: "title 86",
+  },
+  {
+    id: 87,
+    title: "title 87",
+  },
+  {
+    id: 88,
+    title: "title 88",
+  },
+  {
+    id: 89,
+    title: "title 89",
+  },
+  {
+    id: 90,
+    title: "title 90",
+  },
+  {
+    id: 91,
+    title: "title 91",
+  },
+  {
+    id: 92,
+    title: "title 92",
+  },
+  {
+    id: 93,
+    title: "title 93",
+  },
+  {
+    id: 94,
+    title: "title 94",
+  },
+  {
+    id: 95,
+    title: "title 95",
+  },
+  {
+    id: 96,
+    title: "title 96",
+  },
+  {
+    id: 97,
+    title: "title 97",
+  },
+  {
+    id: 98,
+    title: "title 98",
+  },
+  {
+    id: 99,
+    title: "title 99",
+  },
+  {
+    id: 100,
+    title: "title 100",
+  },
+  {
+    id: 101,
+    title: "title 101",
+  },
+  {
+    id: 102,
+    title: "title 102",
+  },
+  {
+    id: 103,
+    title: "title 103",
+  },
+  {
+    id: 104,
+    title: "title 104",
+  },
+  {
+    id: 105,
+    title: "title 105",
+  },
+  {
+    id: 106,
+    title: "title 106",
+  },
+  {
+    id: 107,
+    title: "title 107",
+  },
+  {
+    id: 108,
+    title: "title 108",
+  },
+  {
+    id: 109,
+    title: "title 109",
+  },
+  {
+    id: 110,
+    title: "title 110",
+  },
+  {
+    id: 111,
+    title: "title 111",
+  },
+  {
+    id: 112,
+    title: "title 112",
+  },
+  {
+    id: 113,
+    title: "title 113",
+  },
+  {
+    id: 114,
+    title: "title 114",
+  },
+  {
+    id: 115,
+    title: "title 115",
+  },
+  {
+    id: 116,
+    title: "title 116",
+  },
+  {
+    id: 117,
+    title: "title 117",
+  },
+  {
+    id: 118,
+    title: "title 118",
+  },
+  {
+    id: 119,
+    title: "title 119",
+  },
+  {
+    id: 120,
+    title: "title 120",
+  },
+  {
+    id: 121,
+    title: "title 121",
+  },
+  {
+    id: 122,
+    title: "title 122",
+  },
+  {
+    id: 123,
+    title: "title 123",
+  },
+  {
+    id: 124,
+    title: "title 124",
+  },
+  {
+    id: 125,
+    title: "title 125",
+  },
+  {
+    id: 126,
+    title: "title 126",
+  },
+  {
+    id: 127,
+    title: "title 127",
+  },
+  {
+    id: 128,
+    title: "title 128",
+  },
+  {
+    id: 129,
+    title: "title 129",
+  },
+  {
+    id: 130,
+    title: "title 130",
+  },
+  {
+    id: 131,
+    title: "title 131",
+  },
+  {
+    id: 132,
+    title: "title 132",
+  },
+  {
+    id: 133,
+    title: "title 133",
+  },
+  {
+    id: 134,
+    title: "title 134",
+  },
+  {
+    id: 135,
+    title: "title 135",
+  },
+  {
+    id: 136,
+    title: "title 136",
+  },
+  {
+    id: 137,
+    title: "title 137",
+  },
+  {
+    id: 138,
+    title: "title 138",
+  },
+  {
+    id: 139,
+    title: "title 139",
+  },
+  {
+    id: 140,
+    title: "title 140",
+  },
+  {
+    id: 141,
+    title: "title 141",
+  },
+  {
+    id: 142,
+    title: "title 142",
+  },
+  {
+    id: 143,
+    title: "title 143",
+  },
+  {
+    id: 144,
+    title: "title 144",
+  },
+  {
+    id: 145,
+    title: "title 145",
+  },
+  {
+    id: 146,
+    title: "title 146",
+  },
+  {
+    id: 147,
+    title: "title 147",
+  },
+  {
+    id: 148,
+    title: "title 148",
+  },
+  {
+    id: 149,
+    title: "title 149",
+  },
+  {
+    id: 150,
+    title: "title 150",
+  },
+  {
+    id: 151,
+    title: "title 151",
+  },
+  {
+    id: 152,
+    title: "title 152",
+  },
+  {
+    id: 153,
+    title: "title 153",
+  },
+  {
+    id: 154,
+    title: "title 154",
+  },
+  {
+    id: 155,
+    title: "title 155",
+  },
+  {
+    id: 156,
+    title: "title 156",
+  },
+  {
+    id: 157,
+    title: "title 157",
+  },
+  {
+    id: 158,
+    title: "title 158",
+  },
+  {
+    id: 159,
+    title: "title 159",
+  },
+  {
+    id: 160,
+    title: "title 160",
+  },
+  {
+    id: 161,
+    title: "title 161",
+  },
+  {
+    id: 162,
+    title: "title 162",
+  },
+  {
+    id: 163,
+    title: "title 163",
+  },
+  {
+    id: 164,
+    title: "title 164",
+  },
+  {
+    id: 165,
+    title: "title 165",
+  },
+  {
+    id: 166,
+    title: "title 166",
+  },
+  {
+    id: 167,
+    title: "title 167",
+  },
+  {
+    id: 168,
+    title: "title 168",
+  },
+  {
+    id: 169,
+    title: "title 169",
+  },
+  {
+    id: 170,
+    title: "title 170",
+  },
+  {
+    id: 171,
+    title: "title 171",
+  },
+  {
+    id: 172,
+    title: "title 172",
+  },
+  {
+    id: 173,
+    title: "title 173",
+  },
+  {
+    id: 174,
+    title: "title 174",
+  },
+  {
+    id: 175,
+    title: "title 175",
+  },
+  {
+    id: 176,
+    title: "title 176",
+  },
+  {
+    id: 177,
+    title: "title 177",
+  },
+  {
+    id: 178,
+    title: "title 178",
+  },
+  {
+    id: 179,
+    title: "title 179",
+  },
+  {
+    id: 180,
+    title: "title 180",
+  },
+  {
+    id: 181,
+    title: "title 181",
+  },
+  {
+    id: 182,
+    title: "title 182",
+  },
+  {
+    id: 183,
+    title: "title 183",
+  },
+  {
+    id: 184,
+    title: "title 184",
+  },
+  {
+    id: 185,
+    title: "title 185",
+  },
+  {
+    id: 186,
+    title: "title 186",
+  },
+  {
+    id: 187,
+    title: "title 187",
+  },
+  {
+    id: 188,
+    title: "title 188",
+  },
+  {
+    id: 189,
+    title: "title 189",
+  },
+  {
+    id: 190,
+    title: "title 190",
+  },
+  {
+    id: 191,
+    title: "title 191",
+  },
+  {
+    id: 192,
+    title: "title 192",
+  },
+  {
+    id: 193,
+    title: "title 193",
+  },
+  {
+    id: 194,
+    title: "title 194",
+  },
+  {
+    id: 195,
+    title: "title 195",
+  },
+  {
+    id: 196,
+    title: "title 196",
+  },
+  {
+    id: 197,
+    title: "title 197",
+  },
+  {
+    id: 198,
+    title: "title 198",
+  },
+  {
+    id: 199,
+    title: "title 199",
+  },
+  {
+    id: 200,
+    title: "title 200",
+  },
+];
+
+class Pagination {
+  constructor(data = [], _el_container, _el_pagination, limit = 10) {
+    this.data = data;
+    this._el_container = _el_container;
+    this._el_pagination = _el_pagination;
+    this.limit = limit;
+    this.current_page = 1;
+    this.end_data = this.current_page * this.limit;
+    this.start_data = this.end_data - this.limit;
+    this.total_page = Math.ceil(this.data.length / this.limit);
+    this.pages = [];
+    this.init();
+  }
+  init() {
+    this.printData();
+    this.logicPagination();
+    this.printPagination();
+  }
+  printData() {
+    this._el_container.innerHTML = "";
+    this.data.slice(this.start_data, this.end_data).forEach((i) => {
+      this._el_container.innerHTML += `<div>${i.title}</div>`;
+    });
+  }
+  printPagination() {
+    this._el_pagination.innerHTML = "";
+    const arrow_left = document.createElement("button");
+    arrow_left.innerHTML = "<";
+    this._el_pagination.appendChild(arrow_left);
+    arrow_left.addEventListener("click", () => this.prevPaginate());
+    this.pages.forEach((i) => {
+      if (i.current) {
+        let btn_page = document.createElement("button");
+        btn_page.innerHTML = i.id;
+        btn_page.style.backgroundColor = "green";
+        this._el_pagination.appendChild(btn_page);
+      } else if (!i.current && !i.other) {
+        let btn_page = document.createElement("button");
+        btn_page.innerHTML = i.id;
+        this._el_pagination.appendChild(btn_page);
+        btn_page.addEventListener("click", () => this.paginate(i.id));
+      } else if (i.other) {
+        let btn_page = document.createElement("button");
+        btn_page.innerHTML = "...";
+        btn_page.disabled;
+        this._el_pagination.appendChild(btn_page);
+      }
+    });
+    const arrow_right = document.createElement("button");
+    arrow_right.innerHTML = ">";
+    arrow_right.addEventListener("click", () => this.nextPagination());
+    this._el_pagination.appendChild(arrow_right);
+  }
+  logicPagination() {
+    let other_right = false;
+    let other_left = false;
+    for (let i = 1; i < this.total_page + 1; i++) {
+      if (i === this.current_page) {
+        this.pages.push({ id: i, current: true, other: false });
+      } else if (i <= 3 || i >= this.total_page - 2 || i === this.current_page - 1 || i === this.current_page + 1) {
+        this.pages.push({ id: i, current: false, other: false });
+      } else if (i < this.current_page - 1 && i > 2 && !other_left) {
+        this.pages.push({ id: i, current: false, other: true });
+        other_left = true;
+      } else if (i > this.current_page + 1 && i < this.total_page - 3 && !other_right) {
+        this.pages.push({ id: i, current: false, other: true });
+        other_right = true;
+      }
+    }
+  }
+  prevPaginate() {
+    if (this.current_page > 1) {
+      this.current_page -= 1;
+      this.end_data = this.current_page * this.limit;
+      this.start_data = this.end_data - this.limit;
+      this.pages = [];
+      this.init();
+    }
+  }
+  nextPagination() {
+    if (this.current_page < this.total_page) {
+      this.current_page += 1;
+      this.end_data = this.current_page * this.limit;
+      this.start_data = this.end_data - this.limit;
+      this.pages = [];
+      this.init();
+    }
+  }
+  paginate(pid) {
+    this.current_page = pid;
+    this.end_data = this.current_page * this.limit;
+    this.start_data = this.end_data - this.limit;
+    this.pages = [];
+    this.init();
+  }
+}
+const el_container = document.querySelector(".container");
+const el_pagination = document.querySelector(".pagination");
+new Pagination(data, el_container, el_pagination, 10);
